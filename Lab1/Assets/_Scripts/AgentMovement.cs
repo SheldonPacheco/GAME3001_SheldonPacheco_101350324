96dc6a19aafda1a141022806ab63f3cb91c9e021
Sheldon Pacheco <sheldon.p@outlook.com>
2024-01-16 15:34:06 -0500

    lab2

--- Lab1/Assets/_Scripts/AgentMovement.cs
+++ Lab1/Assets/_Scripts/AgentMovement.cs
@@ -20,18 +20,25 @@ public class AgentMovement : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             m_position = m_Camera.ScreenToWorldPoint(Input.mousePosition);
             m_position.z = 0;
             LookAt2D(m_position);
         }
+        //Vector3 direction = Vector3.right;
+        //Debug.Log(direction);
+        //transform.position = direction* m_speed*Time.deltaTime;
+
         transform.position = Vector3.MoveTowards(transform.position, m_position, m_speed * Time.deltaTime);
+
+        LookAt2D(m_position);
     }
     void LookAt2D(Vector3 pos)
     {
-       
         Vector3 dir = pos - transform.position;  
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
     }
     void OnTriggerEnter2D(Collider2D agent)   
     {
