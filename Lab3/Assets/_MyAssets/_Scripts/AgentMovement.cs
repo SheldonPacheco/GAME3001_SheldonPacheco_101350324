ba252ffd943a27ea8b1787a85e0dc0bc3a69c53e
Sheldon Pacheco <sheldon.p@outlook.com>
2024-02-06 19:23:00 -0500

    lab3

--- Lab3/Assets/_MyAssets/_Scripts/AgentMovement.cs
+++ Lab3/Assets/_MyAssets/_Scripts/AgentMovement.cs
@@ -1,15 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
 
 public class AgentMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    [SerializeField] private Transform island;
-    [SerializeField] private float loadDistance;
-    private Vector3 targetPosition = Vector3.zero;
 
+    private Vector3 targetPosition = Vector3.zero;
+    private GameObject target;
+    Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = new GameObject("target");
+        
+        //Vector2 a = new Vector2 (2.0f, 3.0f);
+        //Vector2 b = new Vector2(10.0f, 12.0f);
+        //Vector2 ab = b - a;
+        // Vector2 n1 = ab.normalized;
+        // Vector2 n2 = ab / MathF.Sqrt(ab.x * ab.x + ab.y *ab.y);
+        // float length1 = n1.magnitude;
+        // float length2 = n2.magnitude;
+        // Debug.Log(n1);
+        // Debug.Log(n2);
+        // Debug.Log(length1);
+        // Debug.Log(length2);
+
+    }
     void Update()
     {
         
@@ -18,28 +38,42 @@ public class AgentMovement : MonoBehaviour
         {
             // Convert mouse position to world position.
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.z = 0f; // Ensure the Z-coordinate is correct for a 2D game  .     
+            targetPosition.z = 0f; // Ensure the Z-coordinate is correct for a 2D game  .
+            target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, 0f);
         }
+        Seek(targetPosition);
+        //Vector3 direction  = (targetPosition - transform.position).normalized;  
 
-        // Move towards the target position.
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        // transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
+        //float distance = Mathf.Min(moveSpeed * Time.deltaTime, (targetPosition - transform.position).magnitude;
+        //transform.position += (targetPosition - transform.position).normalized * moveSpeed * Time.deltaTime;
+
+        //homework change this to a generic seek function
+        //you should supply two gameobjects a seeker and a target
+        // the seeker must contain a rigidbody, amd a maximum speed. all the target needs is a position
+        //Vector2 currentVelocity = rb.velocity;
+        //Vector2 desiredVelocity = direction * moveSpeed;
+        // rb.AddForce(desiredVelocity - currentVelocity);
         // Rotate to look at the target position.
-        LookAt2D(targetPosition);
-
-        // Check island distance.
-        if (Vector3.Distance(transform.position, island.position) < loadDistance)
-        {
-            Debug.Log("Loading end scene...");
-            SceneManager.LoadScene(2);
-        }
+        
     }
+    void Seek(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
 
+        Vector2 currentVelocity = rb.velocity;
+        Vector2 desiredVelocity = direction * moveSpeed;
+
+        rb.AddForce(desiredVelocity - currentVelocity);
+
+        LookAt2D(target);
+    }
     void LookAt2D(Vector3 target)
     {
         Vector3 lookDirection = target - transform.position;
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Euler(0.0f,0.0f, angle);
     }
 }
 
