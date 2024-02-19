e0fd464d7dc9ebd6be8c333f2bcd40ddffbbf708
Sheldon Pacheco <sheldon.p@outlook.com>
2024-02-19 04:54:02 -0500

    changes

--- Lab3/Assets/_MyAssets/_Scripts/Steering.cs
+++ Lab3/Assets/_MyAssets/_Scripts/Steering.cs
@@ -7,9 +7,6 @@ public class Steering
     public static Vector3 Seek(Vector3 seekerPosition, Vector3 seekerVelocity, float moveSpeed, Vector3 targetPosition)
     {
         Vector3 desiredVelocity = (targetPosition - seekerPosition).normalized * moveSpeed;
-
-        // Add other behaviors here if needed
-
         return desiredVelocity;
     }
 }
