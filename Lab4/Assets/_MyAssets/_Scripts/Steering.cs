6eb0f4fa6ed62381467a4f1d28c66d999157b758
Sheldon Pacheco <sheldon.p@outlook.com>
2024-02-19 04:45:23 -0500

    changes

---
+++ Lab4/Assets/_MyAssets/_Scripts/Steering.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering
+{
+    public static Vector3 Seek(Vector3 seekerPosition, Vector3 seekerVelocity, float moveSpeed, Vector3 targetPosition)
+    {
+        Vector3 desiredVelocity = (targetPosition - seekerPosition).normalized * moveSpeed;
+
+        // Add other behaviors here if needed
+
+        return desiredVelocity;
+    }
+}
